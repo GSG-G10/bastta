cb64aa08c924dfcefb9f2d53923c436058f876d3
muath-khatib <mo3az.khatib@hotmail.com>
2021-11-23 23:36:38 +0200

    catching error

--- server/src/controllers/products/productToFav.js
+++ server/src/controllers/products/productToFav.js
@@ -1,11 +1,11 @@
 const { newFavorites } = require('../../database/queries');
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   try {
     const { userId, productId } = req.body;
     newFavorites(userId, productId);
     return res.json({ message: 'Added successfully' });
   } catch (error) {
-    return res.status(500).send('Internal server error');
+    return next(error);
   }
 };
