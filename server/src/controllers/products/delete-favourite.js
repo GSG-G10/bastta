5e9cdf4f63e1e90e6023a5e79a08932c4a49ee89
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-11-25 17:12:05 +0200

    edit productId

--- server/src/controllers/products/delete-favourite.js
+++ server/src/controllers/products/delete-favourite.js
@@ -4,7 +4,7 @@ module.exports = async (req, res, next) => {
   try {
     const { productId } = req.params;
     const { userId } = req.body;
-    if (productId > 0) {
+    if (Number(productId) > 0) {
       const favourite = await findFavById(productId, userId);
       if (!favourite) {
         return res.status(400).json({
