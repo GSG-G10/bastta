a4e711e9e738a388259cc643d3c33a80a9801d99
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-11-25 22:54:58 +0200

    create get profile products controller Relates #37

--- server/src/controllers/products/get-profile-products.js
+++ server/src/controllers/products/get-profile-products.js
@@ -2,8 +2,30 @@ const { profileProductsQuery } = require('../../database/queries');
 
 module.exports = async (req, res, next) => {
   try {
-    const userId = req.params;
-    const data = await profileProductsQuery(userId);
+    const { userId } = req.params;
+    if (Number(userId) > 0) {
+      const data = await profileProductsQuery(userId);
+      const approved = [];
+      const pending = [];
+      if (data) {
+        data.forEach((ele) => {
+          if (ele.approved) {
+            approved.push(ele);
+          } else {
+            pending.push(ele);
+          }
+        });
+      }
+      return res.json({
+        data: {
+          approved,
+          pending: pending.length,
+        },
+      });
+    }
+    return res
+      .status(400)
+      .json({ message: 'Bad Request' });
   } catch (err) {
     next(err);
   }
--- server/src/database/queries/products/profile-products-query.js
+++ server/src/database/queries/products/profile-products-query.js
@@ -1,12 +1,11 @@
 const connection = require('../../connection');
 
-module.exports = (userId) => {
-  const { rows } = connection
+module.exports = async (userId) => {
+  const { rows } = await connection
     .query(`
         SELECT * 
         FROM products 
         WHERE user_id = $1 
-        AND approved = true
     `, [userId]);
   return rows;
 };
