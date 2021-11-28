09a4b435d3a2bd3f6274a6d1cb70bf32cd48e43b
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-11-28 14:08:20 +0200

    create database query to fetch user's favorites Relates #36

--- server/src/database/queries/favourites/index.js
+++ server/src/database/queries/favourites/index.js
@@ -1,7 +1,9 @@
 const findFavById = require('./find-fav');
 const deleteFav = require('./delete-fav');
+const getUserFavs = require('./get-user-favs');
 
 module.exports = {
   deleteFav,
+  getUserFavs,
   findFavById,
 };
