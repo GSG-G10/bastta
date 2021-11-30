5b138cd3c4bc234de1f637f6a1bbb70ca968b752
Ahmad-Omar <aw.omar7@gmail.com>
2021-11-30 19:36:03 +0200

    product card and products containers

---
+++ client/src/components/ProductCard/style.js
@@ -0,0 +1,23 @@
+const cardContent = {
+  px: 2,
+  py: 1.6,
+};
+const cardMedia = {
+  height: {
+    xs: '700px',
+    sm: '400px',
+    md: '300px',
+    lg: '200px',
+  },
+};
+const categoryLable = {
+  position: 'absolute',
+  top: '6px',
+  right: '6px',
+  backgroundColor: '#EEF4FC',
+  py: 0.1,
+  px: 1,
+  borderRadius: '25px',
+};
+
+export { categoryLable, cardContent, cardMedia };
