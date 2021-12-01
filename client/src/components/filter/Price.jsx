b096b05c2e1d17a82edb8e8c720dbb625b5de8bc
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-12-01 06:13:29 +0200

    create price filter component Relates #19

---
+++ client/src/components/filter/Price.jsx
@@ -0,0 +1,30 @@
+import {
+  Radio, RadioGroup, FormControlLabel, FormControl, FormLabel,
+} from '@mui/material';
+import { useSelector, useDispatch } from 'react-redux';
+import { price } from '../../store/actions/filter-actions';
+
+const Price = () => {
+  const { filter } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const handleChange = (e, newValue) => dispatch(price(newValue.split('-')));
+  return (
+    <FormControl component="fieldset" sx={{ width: '40%' }}>
+      <FormLabel component="legend">السعر</FormLabel>
+      <RadioGroup
+        column={true.toString()}
+        aria-label="Price"
+        name="price"
+        value={filter.price}
+        onChange={handleChange}
+        sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+      >
+        <FormControlLabel value="0-199" control={<Radio />} label="0-199" />
+        <FormControlLabel value="200-499" control={<Radio />} label="200-499" />
+        <FormControlLabel value="500-999" control={<Radio />} label="500-999" />
+        <FormControlLabel value="1000+" control={<Radio />} label="1000+" />
+      </RadioGroup>
+    </FormControl>
+  );
+};
+export default Price;
