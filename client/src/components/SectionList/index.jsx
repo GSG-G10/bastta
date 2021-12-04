28b3137d641b46737a1b686a6d41ee505e5913ab
muath-khatib <mo3az.khatib@hotmail.com>
2021-12-04 13:43:45 +0200

    Refactor

--- client/src/components/SectionList/index.jsx
+++ client/src/components/SectionList/index.jsx
@@ -2,29 +2,23 @@ import * as muiModules from '../../mui-modules';
 
 import style from './style';
 
+const links = [
+  'الإلكترونيات',
+  'العقارات',
+  'المركبات',
+  'الأثاث',
+  'الخدمات',
+  'الرياضة',
+  'الأجهزة',
+];
+
 const SectionList = () => (
   <muiModules.Box sx={style.body}>
-    <muiModules.Link sx={style.link} href="/product/search/الإلكترونيات">
-      الإلكترونيات
-    </muiModules.Link>
-    <muiModules.Link sx={style.link} href="/product/search/العقارات">
-      العقارات
-    </muiModules.Link>
-    <muiModules.Link sx={style.link} href="/product/search/المركبات">
-      المركبات
-    </muiModules.Link>
-    <muiModules.Link sx={style.link} href="/product/search/الأثاث">
-      الأثاث
-    </muiModules.Link>
-    <muiModules.Link sx={style.link} href="/product/search/الخدمات">
-      الخدمات
-    </muiModules.Link>
-    <muiModules.Link sx={style.link} href="/product/search/الرياضة">
-      الرياضة
-    </muiModules.Link>
-    <muiModules.Link sx={style.link} href="/product/search/الأجهزة">
-      الأجهزة
-    </muiModules.Link>
+    {links.map((e) => (
+      <muiModules.Link sx={style.link} href={`/product/search/${e}`}>
+        {e}
+      </muiModules.Link>
+    ))}
   </muiModules.Box>
 );
 
