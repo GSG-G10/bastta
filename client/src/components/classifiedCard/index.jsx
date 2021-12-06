3f2d7e66c6dd322448b8f5870ab6b27e594c7655
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-12-06 19:39:34 +0200

    edit filter component Relates #112

--- client/src/components/classifiedCard/index.jsx
+++ client/src/components/classifiedCard/index.jsx
@@ -1,20 +1,16 @@
 import { useParams } from 'react-router-dom';
 import * as M from '../../mui-modules';
-import Filter from '../filter';
 import PaginationClassified from '../paginaion';
 import './style.css';
 
 const CalssifiedCard = () => {
   const { search } = useParams();
   return (
+
     <M.Box width={{ width: '90%' }} className="container-classified">
-      <M.Box className="containers">
-
-        <Filter />
-        <PaginationClassified search={search} />
-
-      </M.Box>
+      <PaginationClassified search={search} />
     </M.Box>
+
   );
 };
 
--- client/src/components/paginaion/index.jsx
+++ client/src/components/paginaion/index.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import propTypes from 'prop-types';
 import * as M from '../../mui-modules';
+import Filter from '../filter';
 
 import './style.css';
 
@@ -31,64 +32,68 @@ const PaginationClassified = ({ search }) => {
   };
 
   return (
-    <M.Box sx={{ width: '60%' }} className="container-classified">
-      {isLoaded
-        ? data?.length > 0
-          ? (data.map((ele) => (
-            <M.Box onClick={() => navigate(`/products/${ele.id}`)} className="sub-container" key={ele.id}>
-              <section className="rihgt">
-                <img className="class-img" src={JSON.parse(ele.images)[0].image_1} alt={ele.type} />
-              </section>
-              <section className="left">
-                <div className="name-classified">
-                  <span className="sub-name-classified">{ele.type}</span>
-                  <button type="submit" className="btn-like-classified">
-                    <M.FavoriteBorderIcon sx={{ color: '#A9AFB0' }} />
-                  </button>
-                </div>
-                <p className="title-classified">
-                  {ele.name}
-                </p>
-                <article className="more-detalies">
-                  <article className="info">
-                    <p className="price">
-                      <span className="sub-price">
-                        {ele.price}
-                        {ele.currency}
-                      </span>
-                    </p>
-                    <p className="icon-city">
-                      <M.LocationOnIcon />
-                      <span className="sub-city">{ele.city}</span>
-                    </p>
+
+    <M.Box className="containers">
+      <Filter setData={setCategories} setIsLoaded={setIsLoaded} setCount={setCount} />
+      <M.Box sx={{ width: '60%' }} className="container-classified">
+        {isLoaded
+          ? data?.length > 0
+            ? (data.map((ele) => (
+              <M.Box onClick={() => navigate(`/products/${ele.id}`)} className="sub-container" key={ele.id}>
+                <section className="rihgt">
+                  <img className="class-img" src={JSON.parse(ele.images)[0].image_1} alt={ele.type} />
+                </section>
+                <section className="left">
+                  <div className="name-classified">
+                    <span className="sub-name-classified">{ele.type}</span>
+                    <button type="submit" className="btn-like-classified">
+                      <M.FavoriteBorderIcon sx={{ color: '#A9AFB0' }} />
+                    </button>
+                  </div>
+                  <p className="title-classified">
+                    {ele.name}
+                  </p>
+                  <article className="more-detalies">
+                    <article className="info">
+                      <p className="price">
+                        <span className="sub-price">
+                          {ele.price}
+                          {ele.currency}
+                        </span>
+                      </p>
+                      <p className="icon-city">
+                        <M.LocationOnIcon />
+                        <span className="sub-city">{ele.city}</span>
+                      </p>
+                    </article>
                   </article>
-                </article>
-                <article className="section-btn-classified">
-                  <button type="button" className="btn-chat-classified">
-                    <M.ChatIcon sx={{ backgroundColor: '#1a6e9a', marginLeft: '12px' }} />
-                    ارسل رسالة
-                  </button>
-                  <button type="button" className="btn-call-classified">
-                    <M.LocalPhoneIcon sx={{ marginLeft: '12px' }} />
-                    <M.Link href={`tel:${ele.phone}`} onClick={(e) => e.stopPropagation()}>
-                      إتصل بالمعلن
-                    </M.Link>
-                  </button>
-                </article>
-              </section>
-            </M.Box>
-          )))
-          : <h2> لا يوجد بيانات لعرضها يرجى المحاولة مرة اخرى </h2>
-        : <h1>يرجى الانتظار ....</h1>}
-      <Pagination
-        count={Math.ceil(count / 10)}
-        variant="outlined"
-        shape="rounded"
-        color="primary"
-        page={page}
-        onChange={handlePageChange}
-        sx={{ display: 'flex', justifyContent: 'center', py: '3rem' }}
-      />
+                  <article className="section-btn-classified">
+                    <button type="button" className="btn-chat-classified">
+                      <M.ChatIcon sx={{ backgroundColor: '#1a6e9a', marginLeft: '12px' }} />
+                      ارسل رسالة
+                    </button>
+                    <button type="button" className="btn-call-classified">
+                      <M.LocalPhoneIcon sx={{ marginLeft: '12px' }} />
+                      <M.Link href={`tel:${ele.phone}`} onClick={(e) => e.stopPropagation()}>
+                        إتصل بالمعلن
+                      </M.Link>
+                    </button>
+                  </article>
+                </section>
+              </M.Box>
+            )))
+            : <h2> لا يوجد بيانات لعرضها يرجى المحاولة مرة اخرى </h2>
+          : <h1>يرجى الانتظار ....</h1>}
+        <Pagination
+          count={Math.ceil(count / 10)}
+          variant="outlined"
+          shape="rounded"
+          color="primary"
+          page={page}
+          onChange={handlePageChange}
+          sx={{ display: 'flex', justifyContent: 'center', py: '3rem' }}
+        />
+      </M.Box>
     </M.Box>
   );
 };
