f9bce7bc2bab6cdd274ffa19dc490e94984e12cb
muath-khatib <mo3az.khatib@hotmail.com>
2021-12-08 16:00:58 +0200

    comment code line assets

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -4,8 +4,8 @@ import types from './types-icons';
 const logo = require('./brand/logo.jpg').default;
 const LogoTransparent = require('./brand/Logo_Transparent.svg').default;
 const bgSlider = require('./bg-slider.jpg').default;
-const code = require('./code.jpg').default;
+// const code = require('./code.jpg').default;
 
 export {
-  types, logo, LogoTransparent, bgSlider, code,
+  types, logo, LogoTransparent, bgSlider,
 };
